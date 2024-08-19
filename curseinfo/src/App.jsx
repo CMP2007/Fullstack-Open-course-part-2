d4c3f0291ca530121ec108e17ce8e6ec55896051
PucheCarlos <puchecarlos00@gmail.com>
2024-08-18 22:39:12 -0400

    exercise 2.1

---
+++ curseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Course = ({course})=>{
+  return(
+    <>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
+const Header = ({title})=>{
+  return(
+    <header>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+const Content = ({parts})=>{
+  return(
+    <ul>
+      {parts.map(part => 
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      )}
+    </ul>
+  )
+}
+
+const Part = ({name, exercises})=>{
+  return(
+    <>
+      <li>{name} {exercises}</li>    
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
